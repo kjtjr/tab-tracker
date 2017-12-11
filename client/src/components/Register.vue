6181eca86599341569d985006a00587e8660d266
Ken Trinka <Ken@KT-SurfacePro3.SCC2012.local>
2017-12-10 21:24:07 -0500

    Set up registration page

---
+++ client/src/components/Register.vue
@@ -0,0 +1,49 @@
+/* 48 minutes into video left off */
+
+
+<template>
+  <div>
+    <h1>Add User</h1>
+    <input
+      type="email"
+      name="email"
+      v-model="email"
+      placeholder="Email" />
+    <br>
+    <input
+      type="password"
+      name="password"
+      v-model="password"
+      placeholder="Password" />
+    <br>
+    <button
+      @click="register">
+      Add User
+      </button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
